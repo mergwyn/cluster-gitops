a4be0a3751d33e8606662aca437895bf905a4031
mergwyn <gary@theclarkhome.com>
2023-11-15 14:12:49 +0000

    Working version

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ release=argo-cd
 values=bootstrap/${namespace}/${release}/values.yaml
 
 helm dependency update --namespace ${namespace} ${chart}/
-Add repo
+TODO Add repo
 helm install --create-namespace --namespace ${namespace} ${release} ${chart}/ --values=${values} #--dry-run --skip-crds
 kubectl apply -f dev-appset.yaml
+
+TODO add secrets
