d718ed8110ccd99a0052afad5132b87ee10690f4
mergwyn <gary@theclarkhome.com>
2024-01-03 08:16:22 +0000

    Correct sabnzbd name

--- helmchart.sh
+++ helmchart.sh
@@ -3,21 +3,8 @@
 set -o nounset
 
 chart=$1
-dirname ${chart}
-basename $(dirname ${chart})
 namespace=$(basename $(dirname ${chart}))
-values=${chart}/values.yaml
-release= # TODO use jq to get release from Chart.yaml
-release=kube-vip
 
-#namespace=kube-system
-#chart=bootstrap/${namespace}/kube-vip
-#values=${chart}/values.yaml
-#release=kube-vip
-
-helm dependency update --namespace ${namespace} ${chart}/
-helm upgrade --create-namespace --namespace ${namespace} ${release} ${chart}/ \
-  --values=settings/settings.yaml \
-  --values=settings/dev.yaml \
-  --values=${values} \
-  --dry-run 
+kubectl kustomize --enable-helm  ${chart} |
+  sed -e "s/namespace: *kube-system/namespace: ${namespace}/" |
+  kubectl apply -n ${namespace} -f - 
