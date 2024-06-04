6365f13048790a48c9f9150e63d0d0cccaec2b99
mergwyn <gary@theclarkhome.com>
2024-06-04 12:31:04 +0100

    disable NFD

--- helmchart.sh
+++ helmchart.sh
@@ -6,5 +6,7 @@ chart=$1
 namespace=$(basename $(dirname ${chart}))
 
 kubectl kustomize --enable-helm  ${chart} |
-  sed -e "s/namespace: *kube-system/namespace: ${namespace}/" |
-  kubectl apply -n ${namespace} -f - 
+  sed -e "s/namespace: *kube-system/namespace: ${namespace}/" | tee /tmp/helmchart |
+  kubectl apply -n ${namespace} -f -
+
+rm -rf ${chart}/charts
